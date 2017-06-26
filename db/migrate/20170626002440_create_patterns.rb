ebfcf722981834544b0204190ccaa933cc4e832a
bauer-hillary-c <hbauer@ezcater.com>
2017-06-25 20:43:14 -0400

    adds to patter migration, sets up association between users and patters

--- db/migrate/20170626002440_create_patterns.rb
+++ db/migrate/20170626002440_create_patterns.rb
@@ -1,6 +1,9 @@
 class CreatePatterns < ActiveRecord::Migration[5.0]
   def change
     create_table :patterns do |t|
+      t.text :title
+      t.text :description
+      t.references :user, index: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170625061116) do
+ActiveRecord::Schema.define(version: 20170626002440) do
+
+  create_table "patterns", force: :cascade do |t|
+    t.text     "title"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_patterns_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
